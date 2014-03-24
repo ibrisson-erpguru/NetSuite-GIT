5e5a9b6944608bdf33b6b3150428debec06dd3fc
ibrisson-erpguru <ianic.brisson@erpguru.com>
2014-03-24 13:40:54 -0400

    Coimitting new PO Process

---
+++ Testing New Setup/PO Process/Scheduled/SS_PO_AutoShip.js
@@ -0,0 +1,15 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       24 Mar 2014     ERPGuru
+ *
+ */
+
+/**
+ * @param {String} type Context Types: scheduled, ondemand, userinterface, aborted, skipped
+ * @returns {Void}
+ */
+function scheduled(type) {
+	nlapiLoadRecord()
+}
